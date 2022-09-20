5440d015727cc46ac864b83363e6964972e5d9d1
Mike <mpuzyrev@tutanota.com>
2022-09-20 14:19:13 +0300

    replace agent

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -2,10 +2,10 @@
 
 import fetch from 'cross-fetch'
 
-export default async (url) => {
+export default async (url, agent=null) => {
   const res = await fetch(url, {
     headers: {
-      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
+      'user-agent': agent || 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
     }
   })
   const status = res.status
