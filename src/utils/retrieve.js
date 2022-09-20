dd2f8c291ecb2b578ae0899d880597057569c937
Mike <mpuzyrev@tutanota.com>
2022-09-21 00:41:48 +0300

    Update retrieve.js

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -2,10 +2,11 @@
 
 import fetch from 'cross-fetch'
 
-export default async (url, agent=null) => {
+export default async (url, opts=null) => {
   const res = await fetch(url, {
+    agent: opts?.agent || null,
     headers: {
-      'user-agent': agent || 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
+      'user-agent': opts.headers || 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
     }
   })
   const status = res.status
